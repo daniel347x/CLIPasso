7b977a7fafc1bae3f89c11bcedf0d6295392469e
Dan Nissenbaum <daniel347x@gmail.com>
2022-11-12 23:11:05 -0500

    Ditto

--- CLIP_/clip/sketch_utils.py
+++ CLIP_/clip/sketch_utils.py
@@ -15,12 +15,21 @@ from torchvision import transforms
 from torchvision.utils import make_grid
 from skimage.transform import resize
 
-from clip.clip import U2NET
+from clip import U2NET
 
 
-def imwrite(img, filename, gamma=2.2, normalize=False, use_wandb=False, wandb_name="", step=0, input_im=None):
+def imwrite(
+    img,
+    filename,
+    gamma=2.2,
+    normalize=False,
+    use_wandb=False,
+    wandb_name="",
+    step=0,
+    input_im=None,
+):
     directory = os.path.dirname(filename)
-    if directory != '' and not os.path.exists(directory):
+    if directory != "" and not os.path.exists(directory):
         os.makedirs(directory)
 
     if not isinstance(img, np.ndarray):
@@ -33,7 +42,7 @@ def imwrite(img, filename, gamma=2.2, normalize=False, use_wandb=False, wandb_na
     if img.ndim == 2:
         # repeat along the third dimension
         img = np.expand_dims(img, 2)
-    img[:, :, :3] = np.power(img[:, :, :3], 1.0/gamma)
+    img[:, :, :3] = np.power(img[:, :, :3], 1.0 / gamma)
     img = (img * 255).astype(np.uint8)
 
     skimage.io.imsave(filename, img, check_contrast=False)
@@ -49,14 +58,14 @@ def plot_batch(inputs, outputs, output_dir, step, use_wandb, title):
     plt.subplot(2, 1, 1)
     grid = make_grid(inputs.clone().detach(), normalize=True, pad_value=2)
     npgrid = grid.cpu().numpy()
-    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation='nearest')
+    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation="nearest")
     plt.axis("off")
     plt.title("inputs")
 
     plt.subplot(2, 1, 2)
     grid = make_grid(outputs, normalize=False, pad_value=2)
     npgrid = grid.detach().cpu().numpy()
-    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation='nearest')
+    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation="nearest")
     plt.axis("off")
     plt.title("outputs")
 
@@ -70,7 +79,7 @@ def plot_batch(inputs, outputs, output_dir, step, use_wandb, title):
 def log_input(use_wandb, epoch, inputs, output_dir):
     grid = make_grid(inputs.clone().detach(), normalize=True, pad_value=2)
     npgrid = grid.cpu().numpy()
-    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation='nearest')
+    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation="nearest")
     plt.axis("off")
     plt.tight_layout()
     if use_wandb:
@@ -86,19 +95,22 @@ def log_sketch_summary_final(path_svg, use_wandb, device, epoch, loss, title):
     canvas_width, canvas_height, shapes, shape_groups = load_svg(path_svg)
     _render = pydiffvg.RenderFunction.apply
     scene_args = pydiffvg.RenderFunction.serialize_scene(
-        canvas_width, canvas_height, shapes, shape_groups)
-    img = _render(canvas_width,  # width
-                  canvas_height,  # height
-                  2,   # num_samples_x
-                  2,   # num_samples_y
-                  0,   # seed
-                  None, # background image
-                  None, # backward_clamp_gradient_mag - required by https://github.com/daniel347x/diffvg fork
-                  *scene_args)
+        canvas_width, canvas_height, shapes, shape_groups
+    )
+    img = _render(
+        canvas_width,  # width
+        canvas_height,  # height
+        2,  # num_samples_x
+        2,  # num_samples_y
+        0,  # seed
+        None,  # background image
+        None,  # backward_clamp_gradient_mag - required by https://github.com/daniel347x/diffvg fork
+        *scene_args,
+    )
 
-    img = img[:, :, 3:4] * img[:, :, :3] + \
-        torch.ones(img.shape[0], img.shape[1], 3,
-                   device=device) * (1 - img[:, :, 3:4])
+    img = img[:, :, 3:4] * img[:, :, :3] + torch.ones(
+        img.shape[0], img.shape[1], 3, device=device
+    ) * (1 - img[:, :, 3:4])
     img = img[:, :, :3]
     plt.imshow(img.cpu().numpy())
     plt.axis("off")
@@ -112,7 +124,7 @@ def log_sketch_summary(sketch, title, use_wandb):
     plt.figure()
     grid = make_grid(sketch.clone().detach(), normalize=True, pad_value=2)
     npgrid = grid.cpu().numpy()
-    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation='nearest')
+    plt.imshow(np.transpose(npgrid, (1, 2, 0)), interpolation="nearest")
     plt.axis("off")
     plt.title(title)
     plt.tight_layout()
@@ -123,14 +135,12 @@ def log_sketch_summary(sketch, title, use_wandb):
 
 def load_svg(path_svg):
     svg = os.path.join(path_svg)
-    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(
-        svg)
+    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(svg)
     return canvas_width, canvas_height, shapes, shape_groups
 
 
 def read_svg(path_svg, device, multiply=False):
-    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(
-        path_svg)
+    canvas_width, canvas_height, shapes, shape_groups = pydiffvg.svg_to_scene(path_svg)
     if multiply:
         canvas_width *= 2
         canvas_height *= 2
@@ -139,18 +149,21 @@ def read_svg(path_svg, device, multiply=False):
             path.stroke_width *= 2
     _render = pydiffvg.RenderFunction.apply
     scene_args = pydiffvg.RenderFunction.serialize_scene(
-        canvas_width, canvas_height, shapes, shape_groups)
-    img = _render(canvas_width,  # width
-                  canvas_height,  # height
-                  2,   # num_samples_x
-                  2,   # num_samples_y
-                  0,   # seed
-                  None, # background image
-                  None, # backward_clamp_gradient_mag - required by https://github.com/daniel347x/diffvg fork
-                  *scene_args)
-    img = img[:, :, 3:4] * img[:, :, :3] + \
-        torch.ones(img.shape[0], img.shape[1], 3,
-                   device=device) * (1 - img[:, :, 3:4])
+        canvas_width, canvas_height, shapes, shape_groups
+    )
+    img = _render(
+        canvas_width,  # width
+        canvas_height,  # height
+        2,  # num_samples_x
+        2,  # num_samples_y
+        0,  # seed
+        None,  # background image
+        None,  # backward_clamp_gradient_mag - required by https://github.com/daniel347x/diffvg fork
+        *scene_args,
+    )
+    img = img[:, :, 3:4] * img[:, :, :3] + torch.ones(
+        img.shape[0], img.shape[1], 3, device=device
+    ) * (1 - img[:, :, 3:4])
     img = img[:, :, :3]
     return img
 
@@ -162,23 +175,23 @@ def plot_attn_dino(attn, threshold_map, inputs, inds, use_wandb, output_path):
     plt.subplot(2, attn.shape[0] + 2, 1)
     main_im = make_grid(inputs, normalize=True, pad_value=2)
     main_im = np.transpose(main_im.cpu().numpy(), (1, 2, 0))
-    plt.imshow(main_im, interpolation='nearest')
-    plt.scatter(inds[:, 1], inds[:, 0], s=10, c='red', marker='o')
+    plt.imshow(main_im, interpolation="nearest")
+    plt.scatter(inds[:, 1], inds[:, 0], s=10, c="red", marker="o")
     plt.title("input im")
     plt.axis("off")
 
     plt.subplot(2, attn.shape[0] + 2, 2)
-    plt.imshow(attn.sum(0).numpy(), interpolation='nearest')
+    plt.imshow(attn.sum(0).numpy(), interpolation="nearest")
     plt.title("atn map sum")
     plt.axis("off")
 
     plt.subplot(2, attn.shape[0] + 2, attn.shape[0] + 3)
-    plt.imshow(threshold_map[-1].numpy(), interpolation='nearest')
+    plt.imshow(threshold_map[-1].numpy(), interpolation="nearest")
     plt.title("prob sum")
     plt.axis("off")
 
     plt.subplot(2, attn.shape[0] + 2, attn.shape[0] + 4)
-    plt.imshow(threshold_map[:-1].sum(0).numpy(), interpolation='nearest')
+    plt.imshow(threshold_map[:-1].sum(0).numpy(), interpolation="nearest")
     plt.title("thresh sum")
     plt.axis("off")
 
@@ -196,29 +209,32 @@ def plot_attn_dino(attn, threshold_map, inputs, inds, use_wandb, output_path):
     plt.close()
 
 
-def plot_attn_clip(attn, threshold_map, inputs, inds, use_wandb, output_path, display_logs):
+def plot_attn_clip(
+    attn, threshold_map, inputs, inds, use_wandb, output_path, display_logs
+):
     # currently supports one image (and not a batch)
     plt.figure(figsize=(10, 5))
 
     plt.subplot(1, 3, 1)
     main_im = make_grid(inputs, normalize=True, pad_value=2)
     main_im = np.transpose(main_im.cpu().numpy(), (1, 2, 0))
-    plt.imshow(main_im, interpolation='nearest')
-    plt.scatter(inds[:, 1], inds[:, 0], s=10, c='red', marker='o')
+    plt.imshow(main_im, interpolation="nearest")
+    plt.scatter(inds[:, 1], inds[:, 0], s=10, c="red", marker="o")
     plt.title("input im")
     plt.axis("off")
 
     plt.subplot(1, 3, 2)
-    plt.imshow(attn, interpolation='nearest', vmin=0, vmax=1)
+    plt.imshow(attn, interpolation="nearest", vmin=0, vmax=1)
     plt.title("atn map")
     plt.axis("off")
 
     plt.subplot(1, 3, 3)
-    threshold_map_ = (threshold_map - threshold_map.min()) / \
-        (threshold_map.max() - threshold_map.min())
-    plt.imshow(threshold_map_, interpolation='nearest', vmin=0, vmax=1)
+    threshold_map_ = (threshold_map - threshold_map.min()) / (
+        threshold_map.max() - threshold_map.min()
+    )
+    plt.imshow(threshold_map_, interpolation="nearest", vmin=0, vmax=1)
     plt.title("prob softmax")
-    plt.scatter(inds[:, 1], inds[:, 0], s=10, c='red', marker='o')
+    plt.scatter(inds[:, 1], inds[:, 0], s=10, c="red", marker="o")
     plt.axis("off")
 
     plt.tight_layout()
@@ -228,13 +244,22 @@ def plot_attn_clip(attn, threshold_map, inputs, inds, use_wandb, output_path, di
     plt.close()
 
 
-def plot_atten(attn, threshold_map, inputs, inds, use_wandb, output_path, saliency_model, display_logs):
+def plot_atten(
+    attn,
+    threshold_map,
+    inputs,
+    inds,
+    use_wandb,
+    output_path,
+    saliency_model,
+    display_logs,
+):
     if saliency_model == "dino":
-        plot_attn_dino(attn, threshold_map, inputs,
-                       inds, use_wandb, output_path)
+        plot_attn_dino(attn, threshold_map, inputs, inds, use_wandb, output_path)
     elif saliency_model == "clip":
-        plot_attn_clip(attn, threshold_map, inputs, inds,
-                       use_wandb, output_path, display_logs)
+        plot_attn_clip(
+            attn, threshold_map, inputs, inds, use_wandb, output_path, display_logs
+        )
 
 
 def fix_image_scale(im):
@@ -243,9 +268,8 @@ def fix_image_scale(im):
     max_len = max(height, width) + 20
     new_background = np.ones((max_len, max_len, 3))
     y, x = max_len // 2 - height // 2, max_len // 2 - width // 2
-    new_background[y: y + height, x: x + width] = im_np
-    new_background = (new_background / new_background.max()
-                      * 255).astype(np.uint8)
+    new_background[y : y + height, x : x + width] = im_np
+    new_background = (new_background / new_background.max() * 255).astype(np.uint8)
     new_im = Image.fromarray(new_background)
     return new_im
 
@@ -253,12 +277,16 @@ def fix_image_scale(im):
 def get_mask_u2net(args, pil_im):
     w, h = pil_im.size[0], pil_im.size[1]
     im_size = min(w, h)
-    data_transforms = transforms.Compose([
-        transforms.Resize(min(320, im_size), interpolation=PIL.Image.BICUBIC),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(
-            0.26862954, 0.26130258, 0.27577711)),
-    ])
+    data_transforms = transforms.Compose(
+        [
+            transforms.Resize(min(320, im_size), interpolation=PIL.Image.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=(0.48145466, 0.4578275, 0.40821073),
+                std=(0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
 
     input_im_trans = data_transforms(pil_im).unsqueeze(0).to(args.device)
 
@@ -268,7 +296,7 @@ def get_mask_u2net(args, pil_im):
         net.load_state_dict(torch.load(model_dir))
         net.to(args.device)
     else:
-        net.load_state_dict(torch.load(model_dir, map_location='cpu'))
+        net.load_state_dict(torch.load(model_dir, map_location="cpu"))
     net.eval()
     with torch.no_grad():
         d1, d2, d3, d4, d5, d6, d7 = net(input_im_trans.detach())
@@ -284,7 +312,7 @@ def get_mask_u2net(args, pil_im):
     mask[mask >= 0.5] = 1
 
     # predict_np = predict.clone().cpu().data.numpy()
-    im = Image.fromarray((mask[:, :, 0]*255).astype(np.uint8)).convert('RGB')
+    im = Image.fromarray((mask[:, :, 0] * 255).astype(np.uint8)).convert("RGB")
     im.save(f"{args.output_dir}/mask.png")
 
     im_np = np.array(pil_im)
